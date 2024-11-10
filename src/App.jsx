a4762c961f76f8b91733e75df66769a3d765c453
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-09 19:21:36 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Content from "./components/Content.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Content(props) {
+  return <p style={{ color: props.color }}>{props.text}</p>;
+}
+
+export default Content;
